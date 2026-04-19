ce65e3b9d329045c1cd0e613da9f5cd21c6a261d
Gopinath G <gopinathgopinath0154@gmail.com>
2026-04-19 09:37:17 +0000

    the terrafrom error fixed

--- starbucks-app-main/terraform/main.tf
+++ starbucks-app-main/terraform/main.tf
@@ -32,39 +32,34 @@ module "eks" {
   version = "~> 21.0"
 
   name               = "starter-eks-cluster"
-  kubernetes_version = "1.33"
+  kubernetes_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+ 
+  enable_cluster_creator_admin_permissions = true
 
   addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {
-      before_compute = true
-    }
-    kube-proxy             = {}
-    vpc-cni                = {
+    coredns = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni = {
+      most_recent    = true
       before_compute = true
     }
   }
 
-  # Optional
-  endpoint_public_access = true
 
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
-
-  vpc_id                   =  module.vpc.vpc_id
-  subnet_ids               =  module.vpc.public_subnets_ids
-  control_plane_subnet_ids =  module.vpc.private_subnets_ids
-
-  # EKS Managed Node Group(s)
   eks_managed_node_groups = {
     example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.medium"]
 
-      min_size     = 2
+      min_size     = 1
       max_size     = 2
-      desired_size = 2
+      desired_size = 1
+
+      ami_type       = "AL2_x86_64"
+      capacity_type  = "ON_DEMAND"
     }
   }
 
